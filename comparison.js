70ff0ae62a389e85565a113d9ed0ebe2af49e2e8
webhero-nur <nurealom522@gmail..com>
2022-09-13 11:35:15 +0600

    comparison understood properly

--- comparison.js
+++ comparison.js
@@ -1,9 +1,17 @@
-var number1 = 5;                    // this is number 5
-var number2 = 5;                    // this is nubmer 5
+var number1 = 5;                    // this is number 5 assign to number1 variable
+var number2 = 5;                    // this is nubmer 5 assign to number2 variable
 var char1 = "5";                    // this is character 5
-var check1 = number1 == number2;    // this will check if the numbers are same or not
+var check1 = number1 == number2;    // this will check if the numbers are same or not. returns true or false boolean result
 console.log(check1);
-var check2 = number1 == char1;      // this will check if the elements are same or not
+var check2 = number1 == char1;      // this will check if the elements are same or not. returns true or false boolean result
 console.log(check2);
-var check3 = number1 === char1;     // this will check if the values are same and same type or not
+var check3 = number1 === char1;     // this will check if the values are same and same type or not. returns true or false boolean result
 console.log(check3);
+var check4 = number1 > number2;     // this will check if the first value grater than the second value. returns true or false boolean result
+console.log(check4);
+var check5 = number1 < number2;     // this will check if the first value less than the second value. returns true or false boolean result
+console.log(check5);
+var check6 = number1 >= number2;    // this will check if the first value grater than or equal to the second value. returns true or false boolean result
+console.log(check6);
+var check7 = number1 <= number2;    // this will check if the first value less than or equal to the second value. returns true or false boolean result
+console.log(check7);
